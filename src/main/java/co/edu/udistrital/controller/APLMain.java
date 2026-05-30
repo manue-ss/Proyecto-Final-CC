f6728c96afde2231bbc03c8b2f2c53d90c899e00
Manuel <juanmasalazarsalazar@gmail.com>
2026-05-29 19:36:42 -0500

    chore: configuración inicial MVC con Maven, JavaFX, MariaDB y Gson

---
+++ src/main/java/co/edu/udistrital/controller/APLMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.udistrital.controller;
+
+/**
+ *
+ * @author Acer-Pc
+ */
+public class APLMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module co.edu.udistrital.auto.rescate {
+    // 1. Dependencias externas (Lo que tu proyecto necesita)
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires org.mariadb.jdbc;
+    requires com.google.gson;
+
+    // 2. Permisos para el Controlador
+    // Exportamos el paquete controller para que JavaFX pueda arrancar APLMain
+    exports co.edu.udistrital.controller;
+    // Abrimos el paquete controller a javafx.fxml para que @FXML funcione en tus controladores
+    opens co.edu.udistrital.controller to javafx.fxml;
+
+    // 3. Permisos para el Modelo (Gson)
+    // Exportamos el modelo por si otras partes de la app lo necesitan
+
+    // Abrimos las entidades a Gson para que pueda convertirlas a JSON mediante Reflexión
+    opens co.edu.udistrital.model.entities to com.google.gson;
+}
