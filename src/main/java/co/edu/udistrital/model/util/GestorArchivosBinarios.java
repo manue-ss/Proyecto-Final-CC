c85b1033353ce71a98e352aa9f12e58509a0ea13
sgnumz <sg8129758@gmail.com>
2026-06-01 16:08:37 -0500

    Gestor de Archivos Binarios

---
+++ src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
@@ -0,0 +1,48 @@
+package co.edu.udistrital.model.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+public class GestorArchivosBinarios<T> {
+
+    public void guardarDatos(String rutaArchivo, T datos) {
+        try {
+            FileOutputStream archivoSalida = new FileOutputStream(rutaArchivo);
+            ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida);
+            
+            canalSalida.writeObject(datos);
+            
+            canalSalida.close();
+            archivoSalida.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public T cargarDatos(String rutaArchivo) {
+        
+        T recuperado = null;
+        
+        try {
+            FileInputStream archivoEntrada = new FileInputStream(rutaArchivo);
+            
+            ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada);
+            
+            recuperado = (T) canalEntrada.readObject();
+            
+            canalEntrada.close();
+            archivoEntrada.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        return recuperado;
+    }
+}
