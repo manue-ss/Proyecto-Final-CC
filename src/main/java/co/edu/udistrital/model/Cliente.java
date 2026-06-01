fb62c0c947237bf2c4380da46d9c2e0f716a872d
acur <acurrr2019@gmail.com>
2026-06-01 11:44:39 -0500

    añadir cliente

---
+++ src/main/java/co/edu/udistrital/model/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.udistrital.model;
+import co.edu.udistrital.model.TipoCliente;
+/**
+ *
+ * @author acurr
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    private TipoCliente tipo;
+
+    public Cliente(int id, String nombre, TipoCliente tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TipoCliente getTipo() {
+        return tipo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
