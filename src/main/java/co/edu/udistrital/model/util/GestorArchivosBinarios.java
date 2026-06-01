a33dabfd962991660f1f0238248cfda91716f857
sgnumz <sg8129758@gmail.com>
2026-06-01 16:13:02 -0500

    Implementacion gestor de archivos binarios

--- src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
+++ src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
@@ -12,37 +12,36 @@ public class GestorArchivosBinarios<T> {
         try {
             FileOutputStream archivoSalida = new FileOutputStream(rutaArchivo);
             ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida);
-            
+
             canalSalida.writeObject(datos);
-            
+
             canalSalida.close();
             archivoSalida.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public T cargarDatos(String rutaArchivo) {
-        
+
         T recuperado = null;
-        
+
         try {
             FileInputStream archivoEntrada = new FileInputStream(rutaArchivo);
-            
             ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada);
-            
+
             recuperado = (T) canalEntrada.readObject();
-            
+
             canalEntrada.close();
             archivoEntrada.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        
+
         return recuperado;
     }
 }
