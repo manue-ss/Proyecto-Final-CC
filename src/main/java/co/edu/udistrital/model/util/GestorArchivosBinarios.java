040861204f66ca6d1291e8179416e1c9923304ee
Manuel <juanmasalazarsalazar@gmail.com>
2026-06-01 23:25:42 -0500

    Parche de Seguridad
    Se implemento un try-with-resources para la apertura y cierre automatico de buferes de datos y escritura, para evitar leaks de memoria.

--- src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
+++ src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
@@ -9,36 +9,29 @@ import java.io.IOException;
 public class GestorArchivosBinarios<T> {
 
     public void guardarDatos(String rutaArchivo, T datos) {
-        try {
-            FileOutputStream archivoSalida = new FileOutputStream(rutaArchivo);
-            ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida);
+        try (FileOutputStream archivoSalida = new FileOutputStream(rutaArchivo); ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida)) {
 
             canalSalida.writeObject(datos);
-
-            canalSalida.close();
-            archivoSalida.close();
-
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @SuppressWarnings("unchecked")
     public T cargarDatos(String rutaArchivo) {
 
         T recuperado = null;
 
-        try {
-            FileInputStream archivoEntrada = new FileInputStream(rutaArchivo);
-            ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada);
+        try (FileInputStream archivoEntrada = new FileInputStream(rutaArchivo); ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada)) {
 
             recuperado = (T) canalEntrada.readObject();
 
-            canalEntrada.close();
-            archivoEntrada.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        }
+        catch (java.io.FileNotFoundException e) {
+            System.out.println("Nota: Archivo " + rutaArchivo + " no existe aún. Se creará al guardar.");
+        }
+        catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
