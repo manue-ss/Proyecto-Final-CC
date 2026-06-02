0ced98d3ea3b2b9fff939b058c2c3ee7cfb9addf
Manuel <juanmasalazarsalazar@gmail.com>
2026-06-02 13:48:50 -0500

    Refactorizacion de rutas de guardado de archivos de memoria

--- src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
+++ src/main/java/co/edu/udistrital/model/util/GestorArchivosBinarios.java
@@ -5,12 +5,25 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GestorArchivosBinarios<T> {
 
-    public void guardarDatos(String rutaArchivo, T datos) {
-        try (FileOutputStream archivoSalida = new FileOutputStream(rutaArchivo); ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida)) {
+    private static final String RUTA_ABSOLUTA = "datos_app/";
 
+    private void asegurarDirectorio() {
+        try {
+            Files.createDirectories(Paths.get(RUTA_ABSOLUTA));
+        }
+        catch (IOException e) {
+            System.err.println("Error creando directorio de datos: " + e.getMessage());
+        }
+    }
+
+    public void guardarDatos(String rutaArchivo, T datos) {
+        asegurarDirectorio();
+        try (FileOutputStream archivoSalida = new FileOutputStream(RUTA_ABSOLUTA + rutaArchivo); ObjectOutputStream canalSalida = new ObjectOutputStream(archivoSalida)) {
             canalSalida.writeObject(datos);
         }
         catch (IOException e) {
@@ -20,13 +33,10 @@ public class GestorArchivosBinarios<T> {
 
     @SuppressWarnings("unchecked")
     public T cargarDatos(String rutaArchivo) {
-
+        asegurarDirectorio();
         T recuperado = null;
-
-        try (FileInputStream archivoEntrada = new FileInputStream(rutaArchivo); ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada)) {
-
+        try (FileInputStream archivoEntrada = new FileInputStream(RUTA_ABSOLUTA + rutaArchivo); ObjectInputStream canalEntrada = new ObjectInputStream(archivoEntrada)) {
             recuperado = (T) canalEntrada.readObject();
-
         }
         catch (java.io.FileNotFoundException e) {
             System.out.println("Nota: Archivo " + rutaArchivo + " no existe aún. Se creará al guardar.");
@@ -34,7 +44,6 @@ public class GestorArchivosBinarios<T> {
         catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-
         return recuperado;
     }
 }
